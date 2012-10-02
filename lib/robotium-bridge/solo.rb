9183849fce606f449247d8415a2a38bcfd1d5cbc
Andrew Vos <andrew.vos@gmail.com>
2012-10-02 13:21:01 +0100

    add clearEditText implementation

--- lib/robotium-bridge/solo.rb
+++ lib/robotium-bridge/solo.rb
@@ -16,25 +16,13 @@ module RobotiumBridge
       ])
     end
 
+    def clear_edit_text index
+      call_method("clearEditText", [{:type => "int", :value => index}])
+    end
+
     def call_method name, arguments
       options = { :body => {:parameters => arguments.to_json} }
       puts HTTParty.post("http://127.0.0.1:7103/execute_method/#{name}", options).body
-    end
-
-    METHODS = [
-      {:name => "clickOnImageButton", :argument_types => ["int"]},
-      {:name => "clearEditText", :argument_types => ["int"]}
-    ]
-
-    METHODS.each do |method|
-      ruby_method_name = method[:name].gsub(/([A-Z])/) { |capture| "_" + capture.downcase }
-      define_method(ruby_method_name) do |*arguments|
-        parameters = method[:argument_types].map do |argument_type|
-          {"type" => argument_type, "value" => arguments.shift}
-        end
-        options = { :body => {:parameters => parameters.to_json} }
-        puts HTTParty.post("http://127.0.0.1:7103/execute_method/#{method[:name]}", options).body
-      end
     end
   end
 end
